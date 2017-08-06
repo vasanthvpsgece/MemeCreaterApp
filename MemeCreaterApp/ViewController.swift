265717ca1662d63da6b8e6ab2adfd369b9674ae7
Vasantha kumar <vasanthvpsgece@gmail.com>
2017-08-06 12:57:18 -0400

    Initial Commit

---
+++ MemeCreaterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeCreaterApp
+//
+//  Created by Vasantha kumar Vijaya kumar on 8/6/17.
+//  Copyright © 2017 Vasantha kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
